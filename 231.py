dc2573ea6eb3771c92c49700a25e9c0eaf0a4193
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2019-12-03 21:33:57 +0530

    20 + 231

---
+++ 231.py
@@ -0,0 +1,36 @@
+"""
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true 
+Explanation: 20 = 1
+Example 2:
+
+Input: 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: 218
+Output: false
+"""
+
+import math
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        
+        def log2(x):
+            return math.log(x)/math.log(2)
+        
+        a = log2(n)
+        
+        return a == int(a)
